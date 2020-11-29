e487c98ae7afe6eb729eef3cb47e07ee5da16f89
Manu Phatak <manu.phatak@procore.com>
2020-11-28 22:13:39 -0800

    drop record syntax

--- src/Day02/Solution.hs
+++ src/Day02/Solution.hs
@@ -5,14 +5,14 @@ import Text.Parsec
 import Text.Parsec.String
 
 part1 :: String -> String
-part1 = show . head . memory . run . setVerb 2 . setNoun 12 . readProgram
+part1 = show . head . programMemory . run . setVerb 2 . setNoun 12 . readProgram
 
 part2 :: String -> String
 part2 = id
 
 type Memory = [Int]
 
-data Program = Program {instructionPointer :: Int, memory :: Memory} deriving (Show)
+data Program = Program Int Memory deriving (Show)
 
 line :: Parser [Int]
 line = number `sepBy` (char ',' *> spaces)
@@ -26,44 +26,41 @@ readProgram input = go (parse line "" input)
     go (Right r) = Program 0 r
 
 showProgram :: Program -> String
-showProgram Program {instructionPointer = _, memory = mem} = intercalate "," (map show mem)
+showProgram (Program _ memory) = intercalate "," (map show memory)
 
 run :: Program -> Program
-run program = runOpCode (memory program !! instructionPointer program) program
+run program@(Program pointer memory) = runOpCode (memory !! pointer) program
 
 runOpCode :: Int -> Program -> Program
-runOpCode 1 program =
-  let addressA = (memory program !! (instructionPointer program + 1))
-      addressB = (memory program !! (instructionPointer program + 2))
-      destination = (memory program !! (instructionPointer program + 3))
-      valueA = (memory program !! addressA)
-      valueB = (memory program !! addressB)
-      nextPointer = instructionPointer program + 4
-      nextMemory = set destination (valueA + valueB) (memory program)
+runOpCode 1 = binary (+)
+runOpCode 2 = binary (*)
+runOpCode 99 = id
+runOpCode opcode = error ("Unknown opcode " ++ show opcode)
+
+binary :: (Int -> Int -> Int) -> Program -> Program
+binary fn (Program pointer memory) =
+  let addressA = (memory !! (pointer + 1))
+      addressB = (memory !! (pointer + 2))
+      destination = (memory !! (pointer + 3))
+      nextPointer = pointer + 4
+      valueA = (memory !! addressA)
+      valueB = (memory !! addressB)
+      nextMemory = set destination (fn valueA valueB) memory
    in run (Program nextPointer nextMemory)
-runOpCode 2 program =
-  let addressA = (memory program !! (instructionPointer program + 1))
-      addressB = (memory program !! (instructionPointer program + 2))
-      destination = (memory program !! (instructionPointer program + 3))
-      valueA = (memory program !! addressA)
-      valueB = (memory program !! addressB)
-      nextPointer = instructionPointer program + 4
-      nextMemory = set destination (valueA * valueB) (memory program)
-   in run (Program nextPointer nextMemory)
-runOpCode 99 program = program
-runOpCode _ program = program
 
 set :: Int -> a -> [a] -> [a]
-set position value list = take position list ++ value : drop (position + 1) list
+set position value = go 0
+  where
+    go _ [] = []
+    go n (x : xs)
+      | n == position = value : go (succ n) xs
+      | otherwise = x : go (succ n) xs
 
 setNoun :: Int -> Program -> Program
-setNoun n program = program {memory = set 1 n (memory program)}
+setNoun n (Program pointer memory) = Program pointer (set 1 n memory)
 
 setVerb :: Int -> Program -> Program
-setVerb n program = program {memory = set 2 n (memory program)}
+setVerb n (Program pointer memory) = Program pointer (set 2 n memory)
 
--- >>> show . head . memory . run . readProgram $ "1,0,0,0,99"
--- "2"
-
--- >>> :t set 1 12
--- set 1 12 :: forall a. Num a => [a] -> [a]
+programMemory :: Program -> Memory
+programMemory (Program _ memory) = memory

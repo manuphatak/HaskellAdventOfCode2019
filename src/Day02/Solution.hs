2c8be7239b531a0d4e67b727ada275c4a00d8a4e
Manu Phatak <manu.phatak@procore.com>
2020-11-28 19:55:13 -0800

    solve part 1

--- src/Day02/Solution.hs
+++ src/Day02/Solution.hs
@@ -1,7 +1,69 @@
 module Day02.Solution where
 
+import Data.List
+import Text.Parsec
+import Text.Parsec.String
+
 part1 :: String -> String
-part1 = id
+part1 = show . head . memory . run . setVerb 2 . setNoun 12 . readProgram
 
 part2 :: String -> String
 part2 = id
+
+type Memory = [Int]
+
+data Program = Program {instructionPointer :: Int, memory :: Memory} deriving (Show)
+
+line :: Parser [Int]
+line = number `sepBy` (char ',' *> spaces)
+  where
+    number = read <$> many digit
+
+readProgram :: String -> Program
+readProgram input = go (parse line "" input)
+  where
+    go (Left _) = Program 0 []
+    go (Right r) = Program 0 r
+
+showProgram :: Program -> String
+showProgram Program {instructionPointer = _, memory = mem} = intercalate "," (map show mem)
+
+run :: Program -> Program
+run program = runOpCode (memory program !! instructionPointer program) program
+
+runOpCode :: Int -> Program -> Program
+runOpCode 1 program =
+  let addressA = (memory program !! (instructionPointer program + 1))
+      addressB = (memory program !! (instructionPointer program + 2))
+      destination = (memory program !! (instructionPointer program + 3))
+      valueA = (memory program !! addressA)
+      valueB = (memory program !! addressB)
+      nextPointer = instructionPointer program + 4
+      nextMemory = set destination (valueA + valueB) (memory program)
+   in run (Program nextPointer nextMemory)
+runOpCode 2 program =
+  let addressA = (memory program !! (instructionPointer program + 1))
+      addressB = (memory program !! (instructionPointer program + 2))
+      destination = (memory program !! (instructionPointer program + 3))
+      valueA = (memory program !! addressA)
+      valueB = (memory program !! addressB)
+      nextPointer = instructionPointer program + 4
+      nextMemory = set destination (valueA * valueB) (memory program)
+   in run (Program nextPointer nextMemory)
+runOpCode 99 program = program
+runOpCode _ program = program
+
+set :: Int -> a -> [a] -> [a]
+set position value list = take position list ++ value : drop (position + 1) list
+
+setNoun :: Int -> Program -> Program
+setNoun n program = program {memory = set 1 n (memory program)}
+
+setVerb :: Int -> Program -> Program
+setVerb n program = program {memory = set 2 n (memory program)}
+
+-- >>> show . head . memory . run . readProgram $ "1,0,0,0,99"
+-- "2"
+
+-- >>> :t set 1 12
+-- set 1 12 :: forall a. Num a => [a] -> [a]
--- test/Day02/SolutionSpec.hs
+++ test/Day02/SolutionSpec.hs
@@ -1,13 +1,37 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day02.SolutionSpec (spec) where
 
+import Data.Foldable (for_)
 import Day02.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  xit "solves Part 1" $ do
+  it "solves Part 1" $ do
     input <- readFile "./test/Day02/input.txt"
-    part1 input `shouldBe` "hello santa"
+    part1 input `shouldBe` "5305097"
   xit "solves Part 2" $ do
     input <- readFile "./test/Day02/input.txt"
     part2 input `shouldBe` "hello santa"
+
+  describe "run" runSpec
+
+data RunSpecCase = RunSpecCase
+  {input :: String, output :: String}
+
+runSpec :: Spec
+runSpec =
+  let cases :: [RunSpecCase]
+      cases =
+        [ RunSpecCase {input = "1,0,0,0,99", output = "2,0,0,0,99"},
+          RunSpecCase {input = "2,3,0,3,99", output = "2,3,0,6,99"},
+          RunSpecCase {input = "2,4,4,5,99,0", output = "2,4,4,5,99,9801"},
+          RunSpecCase {input = "1,1,1,4,99,5,6,0,99", output = "30,1,1,4,2,5,6,0,99"}
+        ]
+
+      test :: RunSpecCase -> Spec
+      test RunSpecCase {..} =
+        it ("converts to " ++ output ++ " given " ++ input) $
+          (showProgram . run . readProgram) input `shouldBe` output
+   in for_ cases test

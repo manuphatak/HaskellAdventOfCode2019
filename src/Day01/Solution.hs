008b7fe98928c98e679332b664dbdb87f5e122dd
Manu Phatak <bionikspoon@gmail.com>
2020-11-28 17:53:02 -0800

    Solve Day 01 (#1)
    * Solve day 01
    
    * setup docker caching
    
    * copy bin scripts from golang repo
    
    * Create Templates
    
    * Tweak the script
    
    * Git check in is optional an opt-in
    
    * Check in the readme
    
    * Setup Day 02
    
    * Inlcude cabal
    
    * tweak script
    
    * rm day 2
    
    * type
    
    * rm layer caching

--- src/Day01/Solution.hs
+++ src/Day01/Solution.hs
@@ -1,7 +1,21 @@
 module Day01.Solution where
 
 part1 :: String -> String
-part1 string = string
+part1 = show . sum . map (fuelRequiredForMass . readInt) . lines
 
 part2 :: String -> String
-part2 string = "Hello World" ++ string
+part2 = show . sum . map (fuelRequiredForMassAndFuel . readInt) . lines
+
+fuelRequiredForMass :: Int -> Int
+fuelRequiredForMass mass = (mass `div` 3) - 2
+
+fuelRequiredForMassAndFuel :: Int -> Int
+fuelRequiredForMassAndFuel = go . fuelRequiredForMass
+  where
+    go :: Int -> Int
+    go fuelRequired
+      | fuelRequired >= 0 = fuelRequired + fuelRequiredForMassAndFuel fuelRequired
+      | otherwise = 0
+
+readInt :: String -> Int
+readInt x = read x :: Int
--- test/Day01/SolutionSpec.hs
+++ test/Day01/SolutionSpec.hs
@@ -1,10 +1,56 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day01.SolutionSpec (spec) where
 
+import Data.Foldable (for_)
 import Day01.Solution
 import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "Part 1" $ do
-    it "does" $ do
-      part1 "hello" `shouldBe` "hello"
+  it "solves Part 1" $ do
+    input <- readFile "./test/Day01/input.txt"
+    part1 input `shouldBe` "3497399"
+  it "solves Part 2" $ do
+    input <- readFile "./test/Day01/input.txt"
+    part2 input `shouldBe` "5243207"
+
+  describe "fuelRequiredForMass" fuelRequiredForMassSpec
+  describe "fuelRequiredForMassAndFuel" fuelRequiredForMassAndFuelSpec
+
+data FuelRequiredCase = FuelRequiredCase
+  {mass :: Int, expected :: Int}
+
+fuelRequiredForMassSpec :: Spec
+fuelRequiredForMassSpec =
+  let cases :: [FuelRequiredCase]
+      cases =
+        [ FuelRequiredCase {mass = 12, expected = 2},
+          FuelRequiredCase {mass = 14, expected = 2},
+          FuelRequiredCase {mass = 1969, expected = 654},
+          FuelRequiredCase {mass = 100756, expected = 33583}
+        ]
+
+      test :: FuelRequiredCase -> Spec
+      test FuelRequiredCase {..} =
+        context ("given a mass of " ++ show mass) $
+          it ("is " ++ show expected) $
+            fuelRequiredForMass mass `shouldBe` expected
+   in for_ cases test
+
+fuelRequiredForMassAndFuelSpec :: Spec
+fuelRequiredForMassAndFuelSpec =
+  let cases :: [FuelRequiredCase]
+      cases =
+        [ FuelRequiredCase {mass = 12, expected = 2},
+          FuelRequiredCase {mass = 14, expected = 2},
+          FuelRequiredCase {mass = 1969, expected = 966},
+          FuelRequiredCase {mass = 100756, expected = 50346}
+        ]
+
+      test :: FuelRequiredCase -> Spec
+      test FuelRequiredCase {..} =
+        context ("given a mass of " ++ show mass) $
+          it ("is " ++ show expected) $
+            fuelRequiredForMassAndFuel mass `shouldBe` expected
+   in for_ cases test

c8741f51687d1d81399ce9406f951daa1b219342
Manu Phatak <manu.phatak@procore.com>
2020-11-29 23:01:41 -0800

    build an input parser

--- src/Day03/Solution.hs
+++ src/Day03/Solution.hs
@@ -1,7 +1,42 @@
 module Day03.Solution where
 
+import Data.Either
+import Text.Parsec
+
 part1 :: String -> String
 part1 = id
 
 part2 :: String -> String
 part2 = id
+
+data Direction = U | R | D | L deriving (Read, Show)
+
+type Distance = Int
+
+data Instruction = Instruction Direction Distance deriving (Show)
+
+type Wire = [Instruction]
+
+closestIntersection :: [String] -> Distance
+closestIntersection = solve . map readWire
+
+solve :: [Wire] -> Int
+solve _ = 0
+
+readWires :: String -> [Wire]
+readWires = map readWire . lines
+
+readWire :: String -> Wire
+readWire input = fromRight ([] :: Wire) $ parse parseWire "" input
+
+parseInstruction :: Parsec String st Instruction
+parseInstruction = Instruction <$> directionParser <*> distanceParser
+  where
+    distanceParser :: Parsec String st Distance
+    distanceParser = read <$> many1 digit
+
+    directionParser :: Parsec String st Direction
+    directionParser = read <$> count 1 (oneOf ['U', 'R', 'D', 'L'])
+
+parseWire :: Parsec String st Wire
+parseWire = parseInstruction `sepBy` char ','
--- test/Day03/SolutionSpec.hs
+++ test/Day03/SolutionSpec.hs
@@ -1,5 +1,6 @@
 module Day03.SolutionSpec (spec) where
 
+import Data.Foldable (for_)
 import Day03.Solution
 import Test.Hspec
 
@@ -8,6 +9,19 @@ spec = do
   xit "solves Part 1" $ do
     input <- readFile "./test/Day03/input.txt"
     part1 input `shouldBe` "hello santa"
+
   xit "solves Part 2" $ do
     input <- readFile "./test/Day03/input.txt"
     part2 input `shouldBe` "hello santa"
+
+  describe "closestIntersection" $ do
+    for_
+      [ (["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"], 159),
+        (["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"], 135)
+      ]
+      test
+  where
+    test :: ([String], Int) -> Spec
+    test (input, expected) =
+      it "solves for the nearest intersection" $
+        closestIntersection input `shouldBe` expected
